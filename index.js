d5c1f12aa8bcf72ad3262f7a4707df46520cc71d
Billerens <degtyarik.ilya@gmail.com>
2022-10-21 03:59:09 +0300

    Info updated + EXPLOSION 💥

--- index.js
+++
@@ -1,9 +0,0 @@
-const onCssPercentClick = () => {
-  const cssPercentElement = document.getElementById('css-percent');
-  const breakableElement = document.getElementById('breakable');
-  const containerElement = document.getElementById('container');
-
-  cssPercentElement.classList.add('animated');
-  containerElement.classList.add('animated');
-  setTimeout(() => breakableElement.classList.add('animated'), 1000);
-}
